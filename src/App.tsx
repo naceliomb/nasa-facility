420f8418f82e4b8d418a90018baa283d9a346a09
Nacelio Barbosa <naceliombdev@gmail.com>
2022-01-26 16:39:41 -0300

    created hero-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './global/styles/reset.css';
 import './assets/styles/index.css';
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
+import HeroContent from './components/HeroContent/HeroContent';
 
 function App() {
   return (
-    <Header />  
+    <section className='main-page'>
+      <Header />
+      <section className='apresentation-project'>
+        <HeroContent />
+      </section>
+
+    </section>
   );
 }
 
---
+++ src/components/HeroContent/HeroContent.tsx
@@ -0,0 +1,22 @@
+function HeroContent(){
+
+    return (
+        <div className="hero">
+            <div className="hero_image-container">
+                <img className='hero_image' />
+            </div>
+            <div className="hero_text-container">
+                <div className="hero_text-title">
+                    <h3>title</h3>
+                </div>
+                <div className="hero_text-content">
+                    <p>TEXT</p>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+
+export default HeroContent;
